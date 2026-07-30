9299264dfbb1b78bf6e00e31eb85615fc37131d6
lucia <laysalvi@gmail.com>
2026-07-30 00:28:44 -0300

    Creación de layout base

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 
-window.Alpine = Alpine;
-
-Alpine.start();
+import './sidebar';
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'ClinData') }}</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    @vite([
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+    ])
+</head>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+<body>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<div class="app-layout">
+
+    <x-layout.sidebar />
+
+    <div class="app-content">
+
+        <x-layout.topbar />
+
+        <main class="page-content">
+
+            {{ $slot }}
+
+        </main>
+
+    </div>
+
+</div>
+
+<script>
+    // Importado desde app.js, pero necesitamos sidebar.js también
+</script>
+
+</body>
 </html>
